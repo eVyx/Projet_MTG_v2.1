7b857526098335c2b4f879730be97823ad0b381a
Matt Bourgoin <88078878+eVyx@users.noreply.github.com>
2022-05-25 15:09:50 +0200

    dsds

--- views/page_inscription.php
+++ views/page_inscription.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../HTML_CSS/page_inscription.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/page_inscription.css"/>
     <title>Subscribe</title>
 </head>
 
